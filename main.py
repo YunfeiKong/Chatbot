318f550e029ba98c9a636f23fa93ea2a2c744419
kyf <kyf@kyf-desktop.bj.intel.com>
2024-08-26 14:41:48 +0800

    [style]: format main.py with formatter

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ app.add_middleware(
     allow_origins=origins,  # 允许访问的源
     allow_credentials=True,  # 支持 cookie
     allow_methods=["*"],  # 允许使用的请求方法
-    allow_headers=["*"]  # 允许携带的 Headers
+    allow_headers=["*"],  # 允许携带的 Headers
 )
 
 app.mount("/static", StaticFiles(directory="web", html=True), name="static")
 
 
 # 当用户上传录音文件，本方法用于接收和处理上传的音频文件
-@app.post('/api/audio_to_audio')
+@app.post("/api/audio_to_audio")
 async def audio_to_audio(audio: UploadFile = File(...), history: str = Form(...)):
-    if audio.filename == '':
+    if audio.filename == "":
         raise HTTPException(status_code=400, detail="No audio part")
     # if not history:
     #     raise HTTPException(status_code=400, detail="No history part")
@@ -40,10 +40,10 @@ async def audio_to_audio(audio: UploadFile = File(...), history: str = Form(...)
         # 读取音频文件
         contents = await audio.read()
 
-        webm_path = './test/input_audio.webm'
-        wav_path = './test/input_audio.wav'
+        webm_path = "./test/input_audio.webm"
+        wav_path = "./test/input_audio.wav"
         try:
-            with open(webm_path, 'wb') as file:
+            with open(webm_path, "wb") as file:
                 # 将字节流写入文件
                 file.write(contents)
         except Exception as e:
@@ -56,17 +56,26 @@ async def audio_to_audio(audio: UploadFile = File(...), history: str = Form(...)
             # TODO parse llm_response
             tts_audio_path = call_tts_api("llm_response")
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error in processing with ASR, LLM, or TTS APIs: {e}")
+            raise HTTPException(
+                status_code=500,
+                detail=f"Error in processing with ASR, LLM, or TTS APIs: {e}",
+            )
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
-    return JSONResponse(content={"asrText": asr_text, "llm_response": llm_response, "ttsAudio": tts_audio_path})
+    return JSONResponse(
+        content={
+            "asrText": asr_text,
+            "llm_response": llm_response,
+            "ttsAudio": tts_audio_path,
+        }
+    )
 
 
 class TextItem(BaseModel):
     text: str
 
 
-@app.post('/api/text_to_audio')
+@app.post("/api/text_to_audio")
 async def text_to_audio(item: TextItem):
     text = item.text
     if not text:
@@ -80,6 +89,7 @@ async def text_to_audio(item: TextItem):
 UPLOAD_DIR = "uploaded_files"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+
 @app.post("/api/upload")
 async def upload_file(file: UploadFile = File(...)):
     try:
@@ -88,19 +98,36 @@ async def upload_file(file: UploadFile = File(...)):
         with open(file_path, "wb") as buffer:
             content = await file.read()
             buffer.write(content)
-        
+
         # 读取文件内容
         with open(file_path, "r", encoding="utf-8") as f:
             file_content = f.read()
-        instruct = '请分析我的运动状态，给出建议。'
+        instruct = """请分析我的运动状态，给出建议。参考标准0分：平衡能力好，建议做稍复杂的全身练习并增加力量性练习，增强体力,提高身体综合素质。
+1-4分：平衡能力开始降低，跌倒风险增大。建议增加提高平衡能力的练习，如单腿跳跃、倒走、太极拳和太
+极剑等。
+5-16分：平衡能力受到较大削弱，跌倒风险较大。建议做针对平衡能力的练习，如单足站立练习、“不倒翁”
+练习、沿直线行走、侧身行走等,适当增加一些力量性练习。
+17-24分：平衡能力较差，很容易跌倒。建议选择合适的助行器并补充钙质，做一些力所能及的简单运动，如
+走楼梯、散步、坐立练习、沿直线行走等，运动时应有人监护以确保安全。"""
         # 模拟调用 LLM 和 TTS API
-        llm_response = call_llm_api(file_content + '请尽可能简要分析我的运动状态，给出一句话作为建议，不需要太长')
+        llm_response = call_llm_api(
+            file_content
+            + "请尽可能简要分析我的运动状态，给出一句话作为建议，不需要太长"
+        )
         tts_audio_path = call_tts_api(llm_response)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error in processing with ASR, LLM, or TTS APIs: {e}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error in processing with ASR, LLM, or TTS APIs: {e}",
+        )
 
-    return JSONResponse(content={"asrText": instruct, "llm_response": llm_response, "ttsAudio": tts_audio_path})
-
+    return JSONResponse(
+        content={
+            "asrText": instruct,
+            "llm_response": llm_response,
+            "ttsAudio": tts_audio_path,
+        }
+    )
 
 
 asr = ASRExecutor()
@@ -109,9 +136,14 @@ llm = ChatModel()
 
 # 先跑一遍ASR+TTS以加快推理速度
 
-tts(text="你好，这是一段测试音频", output="./test/output.wav",
-    am="fastspeech2_csmsc", voc="pwgan_csmsc")
-asr(P('./test/output.wav'))
+tts(
+    text="你好，这是一段测试音频",
+    output="./test/output.wav",
+    am="fastspeech2_csmsc",
+    voc="pwgan_csmsc",
+)
+asr(P("./test/output.wav"))
+
 
 # 模拟的ASR API调用函数
 def call_asr_api(audio_path: str):
@@ -128,7 +160,7 @@ def call_llm_api(text: str):
 # 模拟的TTS API调用函数
 def call_tts_api(text: str):
     # 调用外部TTS服务的代码
-    output_wav_path = './web/wavs/tts.wav'
+    output_wav_path = "./web/wavs/tts.wav"
     tts(text=text, output=output_wav_path, am="fastspeech2_csmsc", voc="pwgan_csmsc")
     return output_wav_path
 
